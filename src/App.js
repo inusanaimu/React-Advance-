113a447f565c85645a250b25534f6ae7e04732c4
inusa naimu <inusanaimu@gmail.com>
2022-05-24 17:06:15 +0100

    controlled Input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
+import React from 'react'
+import Setup from './tutorial/5-useRef/setup/1-useRef-basics'
 
 function App() {
   return (
     <div className='container'>
       <Setup />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -12,18 +12,23 @@ const ControlledInputs = () => {
   // const [email, setEmail] = useState('');
   // const [age, setAge] = useState('');
   // const [people, setPeople] = useState([]);
-  const [person, setPerson] = useState({ firstName: '', email: '', age: '' });
+  const [person, setPerson] = useState({ firstName: '', email: '', age: '' })
+  const [people, setPeople] = useState([])
 
   const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setPerson({ ...person, [name]: value });
-    console.log(name, value);
-  };
+    const name = e.target.name
+    const value = e.target.value
+    setPerson({ ...person, [name]: value })
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+    if (person.firstName && person.email && person.age) {
+      const newPerson = { ...person, id: new Date().getTime().toString() }
+      setPeople([...people, newPerson])
+      setPerson({ firstName: '', email: '', age: '' })
+    }
+  }
   return (
     <>
       <article>
@@ -62,18 +67,19 @@ const ControlledInputs = () => {
             add person
           </button>
         </form>
-        {/* {person.map((person, index) => {
-          const { id, firstName, email } = person;
+        {people.map((person, index) => {
+          const { id, firstName, email, age } = person
           return (
             <div className='item' key={id}>
               <h4>{firstName}</h4>
+              <p>{age}</p>
               <p>{email}</p>
             </div>
-          );
-        })} */}
+          )
+        })}
       </article>
     </>
-  );
-};
+  )
+}
 
-export default ControlledInputs;
+export default ControlledInputs
