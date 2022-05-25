fcb14adf26471e17a79a36d4ad1953fe2d7df132
inusa naimu <inusanaimu@gmail.com>
2022-05-25 17:18:57 +0100

    useReduser

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,72 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
 // reducer function
-
+const reducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload]
+    return {
+      ...state,
+      people: newPeople,
+      showModal: true,
+      modalContent: 'item added',
+    }
+  }
+  if (action.type === 'NO_VALUE') {
+    return { ...state, showModal: true, modalContent: 'Please Enter Value' }
+  }
+  if (action.type === 'CLOSE_MODAL') {
+    return { ...state, showModal: false }
+  }
+  // return state
+  throw new Error('no matching Error type')
+}
+const defaultState = {
+  people: [],
+  showModal: false,
+  modalContent: '',
+}
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  const [name, setName] = useState('')
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
-export default Index;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name }
+      dispatch({ type: 'ADD_ITEM', payload: newItem })
+      setName('')
+    } else {
+      dispatch({ type: 'NO_VALUE' })
+    }
+  }
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' })
+  }
+  return (
+    <>
+      {state.showModal && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
+      <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type='submit'>Add</button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Index
