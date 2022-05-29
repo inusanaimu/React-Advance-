91c7c899d06a953482d563eb220145d754d7084d
inusa naimu <inusanaimu@gmail.com>
2022-05-29 17:08:49 +0100

    React-Router and useParams

--- src/tutorial/11-react-router/setup/People.js
+++ src/tutorial/11-react-router/setup/People.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { data } from '../../../data';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { data } from '../../../data'
+import { Link } from 'react-router-dom'
 const People = () => {
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(data)
   return (
     <div>
       <h1>People Page</h1>
@@ -10,11 +10,12 @@ const People = () => {
         return (
           <div key={person.id} className='item'>
             <h4>{person.name}</h4>
+            <Link to={`/person/${person.id}`}>Learn More</Link>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
 
-export default People;
+export default People
